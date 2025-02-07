e295f4f437a87c33071324f4171667bd147be82e
Catrine <92945935+5k-omar@users.noreply.github.com>
2025-02-08 01:03:34 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -18,18 +18,15 @@ def main():
     print("4. Password Reset")
     
     choice = input("Enter your choice (1 or 2 or 3 or 4): ")
-    
+    Clear()
+
     if choice == "1":
-        Clear()
         AccountCreator.main()
     elif choice == "2":
-        Clear()
         Login.main()
     elif choice == "3":
-        Clear()
         Chat.Chat()
     elif choice == "4":
-        Clear()
         password_reset.PasswordReset()
     else:
         print("Invalid choice. Please enter 1 or 2 or 3 or 4.")
